63e65b4ee25793c1e5cbe108922c46630772d53d
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2023-02-20 15:25:10 +0700

    Update approach

---
+++ Malaria-Detection-Using-Faster-RCNN/EfficientNetv2LTraining.py
@@ -0,0 +1,81 @@
+import numpy as np
+import os
+from keras.applications import EfficientNetV2L
+
+from keras.applications import imagenet_utils
+
+from keras.utils import img_to_array, load_img
+
+import random
+from tqdm import tqdm
+
+from imutils import paths
+
+from sklearn.utils import class_weight
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+
+from sklearn.model_selection import GridSearchCV
+import joblib
+
+# it contains path for each image in our folder
+imagePaths = list(paths.list_images("output/cell_images"))
+random.shuffle(imagePaths)
+
+# it will extract the labels from the path of each image
+labels = [p.split(os.path.sep)[1].split('_')[1].split('.')[0]
+          for p in imagePaths]
+classNames = [str(x) for x in np.unique(labels)]
+
+# loading the EfficientNetV2L pre-trained on imagenet network
+model = EfficientNetV2L(weights="imagenet", include_top=False)
+
+
+# list which will have 81,920 featurs for each image
+data = []
+for i in tqdm(imagePaths):
+    image = load_img(i, target_size=(256, 256))  # loading image by there paths
+    image = img_to_array(image)  # converting images into arrays
+    # inserting a new dimension because keras need extra dimensions
+    image = np.expand_dims(image, axis=0)
+    # preprocessing image according to imagenet data
+    image = imagenet_utils.preprocess_input(image)
+    features = model.predict(image)  # extracting those features from the model
+    data.append(features)  # appending features to the list
+
+data = np.array(data)  # converting list into array
+data = data.reshape(data.shape[0], 1280*8*8)
+
+print(model.summary())
+
+
+class_weights = class_weight.compute_class_weight(class_weight='balanced',
+                                                  classes=np.unique(labels),
+                                                  y=labels)
+weights = {}
+for i in range(5):
+    weights[i] = class_weights[i]
+
+# convert the labels from integers to vectors
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+
+print(le.classes_)
+
+# hyper-parameter tuning parameters for logistic regression
+params = {"C": [1.0, 10.0, 100.0]}
+
+model = GridSearchCV(estimator=SVC(class_weight=weights,
+                     verbose=1), param_grid=params, cv=5, verbose=2)
+model.fit(data, labels)
+
+print(model.cv_results_)
+print("The best classifier is: ", model.best_estimator_)
+
+if not os.path.exists('output/models'):
+    os.makedirs('output/models')
+# Save the model as a pickle in a file
+joblib.dump(model.best_estimator_, 'output/models/model_SVC.pkl')
+
+
+
---
+++ Malaria-Detection-Using-Faster-RCNN/testCellExtraction.py
@@ -0,0 +1,59 @@
+from sklearn.metrics import classification_report
+import pandas as pd
+import numpy as np
+from imutils import paths
+import os
+
+from PIL import Image
+import pandas as pd
+import numpy as np
+from imutils import paths
+import os
+import random
+
+import joblib
+# importing Keras implementation of the pre-trained VGG16 network
+from keras.applications import EfficientNetV2L
+# Utilities for ImageNet data preprocessing & prediction decoding
+from keras.applications import imagenet_utils
+
+import numpy as np
+from PIL import Image
+
+
+imagePaths = list(paths.list_images("output/cell_images"))
+svc_model = joblib.load('output/models/model_SVC.pkl')
+
+print(svc_model.best_estimator_)
+
+efficientnet_model = EfficientNetV2L(weights="imagenet", include_top=False)
+
+classes = ['gametocyte', 'leukocyte', 'ring', 'schizont', 'trophozoite']
+labels = []
+targets = []
+
+for i in imagePaths:
+    targets.append(i.split(os.path.sep)[1].split('_')[1].split('.')[0])
+
+    im = Image.open(i)
+    cr_img = im.resize((256, 256))
+
+    data = np.array(cr_img)
+    data = np.expand_dims(data, axis=0)
+
+    data = imagenet_utils.preprocess_input(data)
+
+    data = efficientnet_model.predict(data)
+
+    data = np.array(data)
+    data = data.reshape(1, 1280 * 8 * 8)
+
+    pred = svc_model.predict(data)
+
+    label = classes[pred[0]]
+    labels.append(label)
+
+final_targets = [classes.index(i) for i in targets]
+final_labels = [classes.index(i) for i in labels]
+
+print(classification_report(final_targets, final_labels, target_names=classes))
