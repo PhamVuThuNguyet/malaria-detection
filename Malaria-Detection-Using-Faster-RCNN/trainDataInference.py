f93d54a4c7c7bae47b50ff28d4472f8daf6b18ca
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2023-02-26 13:04:47 +0700

    Update code

--- Malaria-Detection-Using-Faster-RCNN/trainDataInference.py
+++ Malaria-Detection-Using-Faster-RCNN/trainDataInference.py
@@ -15,10 +15,26 @@ from object_detection.utils import label_map_util
 import tensorflow as tf
 import numpy as np
 
-import cv2
 from PIL import Image
 
+
+def load_image_into_numpy_array(path):
+    """Load an image from file into a numpy array.
+
+    Puts image into numpy array to feed into tensorflow graph.
+    Note that by convention we put it into a numpy array with shape
+    (height, width, channels), where channels=3 for RGB.
+
+    Args:
+      path: the file path to the image
+
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3)
+    """
+    return np.array(Image.open(path))
+
 # Inference on all training data
+
 
 train = pd.read_json('../data/malaria_bbbc_80k/malaria/training.json')
 
@@ -37,7 +53,7 @@ for i in tqdm(range(train.shape[0])):
 df_train = pd.DataFrame(
     data, columns=['img_name', 'label', 'x_min', 'y_min', 'x_max', 'y_max'])
 
-non_rbc = ['trophozoite', 'schizont', 'ring', 'gametocyte', 'leukocyte']
+non_rbc = ['gametocyte', 'leukocyte', 'ring', 'schizont', 'trophozoite']
 
 # converting all cells other than rbc to non-rbc
 for i in range(df_train.shape[0]):
@@ -45,7 +61,7 @@ for i in range(df_train.shape[0]):
         df_train.iloc[i, 1] = 'non_rbc'
 
 df_train.img_name = df_train.img_name.apply(
-    lambda x: "input/training_images/"+str(x))
+    lambda x: "annotated_data/training_images/"+str(x))
 
 
 # dataframe with only two labels RBC and NON-RBC for FasterRCNN Detector
@@ -73,29 +89,20 @@ df_train.img_name = df_train.img_name.apply(
 # dataframe with all labels
 df_train_all = df_train[df_train['label'] != "difficult"]
 
-# laoding trained model and classes for faster-rcnn ||already trained and saved on disk
+# loading trained model and classes for faster-rcnn ||already trained and saved on disk
 # saved Faster-RCNN model graph
-modelPath = "output/models/saved_model/saved_model.pb"
+modelPath = "output/models/frcnn_3/saved_model"
 labels_loc = "output/records/classes.pbtxt"  # saved classes files
 min_confidence = 0.5
-num_classes = 2
 
-colors = np.float64(np.array([[255, 1, 1],
-                             [86, 1, 255],
-                              [1, 231, 255],
-                              [1, 255, 61],
-                              [214, 255, 1],
-                              [255, 120, 1]]))
 
 training_images = np.unique(df_train_all.img_name.values)
 
-svc_model = joblib.load('output/models/model_SVC.pkl')
-model = tf.saved_model.load('output/models/saved_model')
+svc_model = joblib.load('output/models/EfficientNet-SVM/model_SVC_4.pkl')
 
-labelMap = label_map_util.load_labelmap(labels_loc)
-categories = label_map_util.convert_label_map_to_categories(
-    labelMap, max_num_classes=num_classes, use_display_name=True)
-categoryIdx = label_map_util.create_category_index(categories)
+detect_fn = tf.saved_model.load(modelPath)
+categoryIdx = label_map_util.create_category_index_from_labelmap(labels_loc,
+                                                                 use_display_name=True)
 
 classes = ['gametocyte', 'leukocyte', 'ring', 'schizont', 'trophozoite']
 
@@ -103,104 +110,116 @@ train_prediction_rnn = {}
 training_predicition_efficientnet = {}
 predicted_boxes_stacked_train = defaultdict(dict)
 
-with model.as_default():
-    with tf.Session(graph=model) as sess:
 
-        imageTensor = model.get_tensor_by_name("image_tensor:0")
-        boxesTensor = model.get_tensor_by_name("detection_boxes:0")
-        scoresTensor = model.get_tensor_by_name("detection_scores:0")
-        classesTensor = model.get_tensor_by_name("detection_classes:0")
-        numDetections = model.get_tensor_by_name("num_detections:0")
+efficientnet_model = EfficientNetV2L(
+    weights="imagenet", include_top=False)
 
-        with tf.Session() as sess2:
+for img in tqdm(training_images):
+    print('Running inference for {}... '.format(img), end='')
 
-            efficientnet_model = EfficientNetV2L(
-                weights="imagenet", include_top=False)
+    image_np = load_image_into_numpy_array(img)
 
-            for img in tqdm(training_images):
-                image = cv2.imread(img)
-                (H, W) = image.shape[:2]
+    (H, W) = image_np.shape[:2]
 
-                output = image.copy()
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
 
-                image = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
-                image = np.expand_dims(image, axis=0)
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detect_fn(input_tensor)
 
-                (boxes, scores, labels, N) = sess.run(
-                    [boxesTensor, scoresTensor, classesTensor, numDetections], feed_dict={imageTensor: image})
-                boxes = np.squeeze(boxes)
-                scores = np.squeeze(scores)
-                labels = np.squeeze(labels)
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
 
-                o = []
-                boxes_nm = []
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(
+        np.int64)
 
-                for (box, score, label) in zip(boxes, scores, labels):
-                    if score < 0.4:
-                        continue
-                    (startY, startX, endY, endX) = box
+    boxes = detections['detection_boxes']
 
-                    startX = int(startX*W)
-                    startY = int(startY*H)
-                    endX = int(endX*W)
-                    endY = int(endY*H)
+    scores = detections['detection_scores']
 
-                    if img in predicted_boxes_stacked_train:
-                        predicted_boxes_stacked_train[img]['boxes'].append(
-                            [startX, startY, endX, endY])
-                        predicted_boxes_stacked_train[img]['scores'].append(
-                            score)
-                    else:
-                        predicted_boxes_stacked_train[img]['boxes'] = [
-                            [startX, startY, endX, endY]]
-                        predicted_boxes_stacked_train[img]['scores'] = [score]
+    labels = detections['detection_classes']
 
-                    if categoryIdx[label]['name'] == "non_rbc":
+    # boxes = np.squeeze(boxes)
+    # scores = np.squeeze(scores)
+    # labels = np.squeeze(labels)
 
-                        if categoryIdx[label]['name'] in train_prediction_rnn:
-                            train_prediction_rnn[categoryIdx[label]
-                                                 ['name']] += 1
-                        else:
-                            train_prediction_rnn[categoryIdx[label]
-                                                 ['name']] = 1
+    o = []
+    boxes_nm = []
 
-                        b_box = [startX, startY, endX, endY]
+    for (box, score, label) in zip(boxes, scores, labels):
+        if score < min_confidence:
+            continue
+        (startY, startX, endY, endX) = box
 
-                        im = Image.open(img)
-                        cr_img = im.crop(b_box)
-                        cr_img = cr_img.resize((256, 256))
+        startX = int(startX*W)
+        startY = int(startY*H)
+        endX = int(endX*W)
+        endY = int(endY*H)
 
-                        data = np.array(cr_img)
-                        data = np.expand_dims(data, axis=0)
-                        data = imagenet_utils.preprocess_input(data)
+        if img in predicted_boxes_stacked_train:
+            predicted_boxes_stacked_train[img]['boxes'].append(
+                [startX, startY, endX, endY])
+            predicted_boxes_stacked_train[img]['scores'].append(
+                score)
+        else:
+            predicted_boxes_stacked_train[img]['boxes'] = [
+                [startX, startY, endX, endY]]
+            predicted_boxes_stacked_train[img]['scores'] = [score]
 
-                        data = efficientnet_model.predict(data)
+        if categoryIdx[label]['name'] == "non_rbc":
 
-                        data = np.array(data)
-                        data = data.reshape(1, 1280 * 8 * 8)
+            if categoryIdx[label]['name'] in train_prediction_rnn:
+                train_prediction_rnn[categoryIdx[label]
+                                     ['name']] += 1
+            else:
+                train_prediction_rnn[categoryIdx[label]
+                                     ['name']] = 1
 
-                        pred = svc_model.predict(data)
-                        label = classes[pred[0]]
+            b_box = [startX, startY, endX, endY]
 
-                        if label in training_predicition_efficientnet:
-                            training_predicition_efficientnet[label] += 1
-                        else:
-                            training_predicition_efficientnet[label] = 1
+            im = Image.open(img)
+            cr_img = im.crop(b_box)
+            cr_img = cr_img.resize((256, 256))
 
-                    else:
-                        label = categoryIdx[label]
-                        idx = int(label["id"])-1
-                        label = label['name']
+            data = np.array(cr_img)
+            data = np.expand_dims(data, axis=0)
+            data = imagenet_utils.preprocess_input(data)
 
-                        if label in train_prediction_rnn:
-                            train_prediction_rnn[label] += 1
-                        else:
-                            train_prediction_rnn[label] = 1
+            data = efficientnet_model.predict(data)
 
-                        if label in training_predicition_efficientnet:
-                            training_predicition_efficientnet[label] += 1
-                        else:
-                            training_predicition_efficientnet[label] = 1
+            data = np.array(data)
+            data = data.reshape(1, 1280 * 8 * 8)
+
+            pred = svc_model.predict(data)
+            label = classes[pred[0]]
+
+            if label in training_predicition_efficientnet:
+                training_predicition_efficientnet[label] += 1
+            else:
+                training_predicition_efficientnet[label] = 1
+
+        else:
+            label = categoryIdx[label]
+            idx = int(label["id"])-1
+            label = label['name']
+
+            if label in train_prediction_rnn:
+                train_prediction_rnn[label] += 1
+            else:
+                train_prediction_rnn[label] = 1
+
+            if label in training_predicition_efficientnet:
+                training_predicition_efficientnet[label] += 1
+            else:
+                training_predicition_efficientnet[label] = 1
 
 
 print("Ground Truth for F-RCNN::", dict(df_train_two.label.value_counts()))
@@ -210,3 +229,7 @@ print("Ground Truth for Finetuned EfficientNet::",
       dict(df_train_all.label.value_counts()))
 print("Prediction for Finetuned EfficientNet::",
       training_predicition_efficientnet)
+
+import json
+with open('./output/records/predicted_boxes_stacked_train.json', 'w+') as fp:
+    json.dump(str(predicted_boxes_stacked_train), fp)
